f1422b0487240b1f6ce2fcb21b041c9fdd186238
Venkat Chinni <venkat.chinni@mx43wfjg5l.rackspace.corp>
2018-11-20 13:43:22 -0600

    Adding thead dynamically

--- app/__init__.py
+++ app/__init__.py
@@ -91,5 +91,3 @@ class users(db.Model):
 
     def as_dict(self):
         return {c.name:getattr(self,c.name) for c in self.__table__.columns}
-
-db.init_app(app)
--- app/static/js/custom.js
+++ app/static/js/custom.js
@@ -1,5 +1,4 @@
-$('#searchflight').click(function(e) {
-//Innerhtml to replace with flight thead
+$('#searchflight').click(function (e) {
     e.preventDefault();
     $.ajax({
         type: "GET",
@@ -8,13 +7,36 @@ $('#searchflight').click(function(e) {
             from: $("#from-place").val(), // < note use of 'this' here
 
         },
-        success: function(result) {
+        success: function (result) {
 
-            console.log(result)
-            var obj = JSON.parse(result)
+            console.log(result);
+            var table_template = `
+                <table id="searchtable" class="table" align="center">
+                    <thead class="black white-text">
+                        <tr>
+                            <th scope="col" data-field="id">id</th>
+                            <th scope="col" data-field="airlines">airlines</th>
+                            <th scope="col" data-field="flight_no" >flight_no</th>
+                            <th scope="col" data-field="source" >source</th>
+                            <th scope="col" data-field="destination" >destination</th>
+                            <th scope="col" data-field="starttime" >starttime </th>
+                            <th scope="col" data-field="endtime" >   endtime   </th>
+                            <th scope="col" data-field="amount"> amount </th>
+                        </tr>
+                    </thead>
+                </table>`;
+            console.log("loading of thead heheh")
+            console.log(typeof result)
+            $('#searchresponse').html(table_template);
+
+            // var obj = JSON.parse(result);
+            $('#searchtable').bootstrapTable({
+                result: JSON.parse(result)
+            });
+
         },
 
-        error: function(result) {
+        error: function (result) {
             console.log("error")
         }
     });
