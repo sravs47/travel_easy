cb6e4306c264cae9840b99b39e1b7fdab90dcd11
Venkat Chinni <venkat.chinni@mx43wfjg5l.rackspace.corp>
2018-11-20 19:44:56 -0600

    Working integration of flights from front end to backend

--- app/run.py
+++ app/run.py
@@ -4,62 +4,67 @@ import json
 from flask import render_template, session, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 
-from app import app, flight_listings, users,hotel_listings
+from app import app, flight_listings, users, hotel_listings
 from app.helpers import utils
 from app.helpers.loginHelper import is_logged_in
 from app.helpers.registerHelper import RegisterForm
 
-db=SQLAlchemy(app)
+db = SQLAlchemy(app)
+
 
 @app.route('/')
 def default():
     return render_template('index.html')
 
+
 @app.route('/index')
 def index():
     return render_template('index.html')
+
 
 @app.route('/blog')
 def blog():
     return render_template('blog.html')
 
-@app.route('/register',methods=['GET','POST'])
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm(request.form)
     print(request)
-    a=request
+    a = request
     if request.method == 'POST':
         if form.validate():
-            result = users(request.form['firstname'],request.form['lastname'],request.form['rusername'],request.form['rpassword'],request.form['email'],datetime.datetime.now())
+            result = users(request.form['firstname'], request.form['lastname'], request.form['rusername'],
+                           request.form['rpassword'], request.form['email'], datetime.datetime.now())
             db.session.add(result)
             db.session.commit()
-            session['logged_in']=True
-            session['username']=request.form['rusername']
+            session['logged_in'] = True
+            session['username'] = request.form['rusername']
             flash('You are now registered and can log in', 'success')
             return redirect(url_for('index'))
         else:
-            flash('Verify all the fields properly','danger')
+            flash('Verify all the fields properly', 'danger')
     return render_template('register.html')
 
 
-@app.route('/login',methods=['GET','POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method=='POST':
+    if request.method == 'POST':
         username = request.form['username']
-        password_candidate=request.form['password']
+        password_candidate = request.form['password']
         if users.query.filter_by(username=username, password=password_candidate).first() is not None:
-            session['logged_in']=True
-            session['username']=username
-            flash('You are now logged in','success')
+            session['logged_in'] = True
+            session['username'] = username
+            flash('You are now logged in', 'success')
             return redirect(url_for('index'))
         else:
-            error='Invalid login'
-            return render_template('register.html',error=error)
+            error = 'Invalid login'
+            return render_template('register.html', error=error)
 
 
     else:
-        error='Username not found'
-        return render_template('register.html',error)
+        error = 'Username not found'
+        return render_template('register.html', error)
     return render_template('register.html')
 
 
@@ -67,18 +72,24 @@ def login():
 @is_logged_in
 def logout():
     session.clear()
-    flash('You are now logged out','success')
+    flash('You are now logged out', 'success')
     return redirect(url_for('index'))
+
 
 @app.route('/api/flights')
 def getflights():
-    return json.dumps([r.as_dict()for r in flight_listings.query.all()],default = utils.datetimeconverter)
+    args = request.args
+    print(args)
+    return json.dumps([r.as_dict() for r in flight_listings.query.filter(
+        flight_listings.starttime > datetime.datetime.strptime(args['starttime'],'%m/%d/%Y'),
+        flight_listings.source == args['from'], flight_listings.destination == args['to'])],
+                      default=utils.datetimeconverter)
+
 
 @app.route('/api/hotels')
 def gethotels():
-    return json.dumps([r.as_dict() for r in hotel_listings.query.all()],default=utils.datetimeconverter)
+    return json.dumps([r.as_dict() for r in hotel_listings.query.all()], default=utils.datetimeconverter)
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
-
--- app/static/js/custom.js
+++ app/static/js/custom.js
@@ -1,31 +1,76 @@
 $('#searchflight').click(function (e) {
-    e.preventDefault();
+    $('html,body').animate({
+            scrollTop: $("#fh5co-tours").offset().top
+        },
+        'slow');
+    $('#searchresponse').bootstrapTable({
+        method: 'get',
+        url: '/api/flights',
+        cache: false,
+        striped: false,
+        queryParams: function (params) {
+            return {
+                from: $("#from-place").val(),
+                to: $("#to-place").val(),
+                starttime: $('#date-start').val(),
+                endtime: $('#date-end').val(),
+                count: $('#adultcount').val() + $('#childcount').val()
+            }
+        },
+        clickToSelect: true,
+        columns: [{
+            field: 'radio',
+            radio: 'true'
+        },
+            {
+                field: 'id',
+                title: 'ID',
+                align: 'center',
+                visible: false
+            }, {
+                field: 'airlines',
+                title: 'AIRLINES'
+
+            }, {
+                field: 'flight_no',
+                title: 'FLIGHT NUMBER'
+            }, {
+                field: 'source',
+                title: 'SOURCE'
+            }, {
+                field: 'destination',
+                title: 'DESTINATION'
+            }, {
+                field: 'starttime',
+                title: 'STARTTIME'
+            }, {
+                field: 'endtime',
+                title: 'ENDTIME'
+            }, {
+                field: 'amount',
+                title: 'AMOUNT'
+            }],
+        rowStyle: function rowStyle(row, index) {
+            return {
+                classes: 'text-nowrap another-class',
+                css: {"color": "#4E443C", "font-size": "25px","font-family":"Georgia,serif"}
+            };
+        },
+        onLoadSuccess: function () {
+            console.log($('#searchresponse').html());
+        }
+    });
 
     var table_template = `
-                <table id="searchtable" class="table" align="center">
-                    <thead class="table table-hover table-striped ">
-                        <tr>
-                            <th scope="col" data-field="id">id</th>
-                            <th scope="col" data-field="airlines">airlines</th>
-                            <th scope="col" data-field="flight_no" >flight_no</th>
-                            <th scope="col" data-field="source" >source</th>
-                            <th scope="col" data-field="destination" >destination</th>
-                            <th scope="col" data-field="starttime" >starttime </th>
-                            <th scope="col" data-field="endtime" >   endtime   </th>
-                            <th scope="col" data-field="amount"> amount </th>
-                        </tr>
-                    </thead>
-                </table>`;
-    console.log("loading of thead heheh")
-
-    $('#searchresponse').html(table_template);
-
-
-    // var obj = JSON.parse(result);
-    $.get("/api/flights", function (data, status) {
-        console.log(data)
-        $('#searchtable').bootstrapTable({
-            data: JSON.parse(data)
-        });
+            <div class="col-xs-12 col-xs-2">
+                <input type="submit" id="selectflight" class="btn btn-primary btn-block" value="Book Flight">
+            </div>
+        `;
+    $('#bookflightplaceholder').html(table_template);
+    // console.log($('#searchresponse').html());
+    $('#selectflight').click(function () {
+        console.log($('#searchresponse').bootstrapTable('getSelections')[0]);
     });
+
 });
+
