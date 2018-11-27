1dad537836a9554aae28e367edd4d10b66593d19
venkat chinni <venkat.chinni@rackspace.com>
2018-11-26 20:58:50 -0600

    Refactoring the app and making it runnable from terminal

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='travel_easy'
+    name='travel_easy',
+    packages=['app'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+
 )
