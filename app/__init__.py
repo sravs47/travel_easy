ff1cd708430148e7eb83c1dfdb854f546d0ad59f
sravs47 <sravani@venkats-MacBook-Pro.local>
2020-04-03 13:27:53 -0500

    removing unused folders

--- app/__init__.py
+++ app/__init__.py
@@ -253,6 +253,17 @@ def flightstatus():
     return render_template('flightStatus.html')
 
 
+@app.route('/api/flights')
+def getflights():
+    args = request.args
+    output = json.dumps([r.as_dict() for r in flight_listings.query.filter(
+        flight_listings.starttime == datetime.datetime.strptime(args['starttime'], '%m/%d/%Y'),
+        flight_listings.seatcount >= int(args['count']),
+        flight_listings.source == args['from'], flight_listings.destination == args['to'])],
+                        default=utils.datetimeconverter)
+    print(output)
+    return output
+
 
 @app.route('/api/hotels')
 def gethotels():
